781376a2537bb15e2c7e245a392195a202e2f900
emotional-push <sharma.devanshu089@gmail.com>
2025-07-25 10:53:30 +0530

    Updated the main file and added base program no errors found

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let number_of_day = 4;
+    match number_of_day {
+        1 => print!("Monday"),
+        2 => print!("Tuesday"),
+        3 => print!("Wednesday"),
+        4 => print!("Thursday"),
+        5 => print!("Friday"),
+        6 => print!("Saturday"),
+        7 => print!("Sunday"),
+        _ => print!("Invalid Date"),
+    }
 }
